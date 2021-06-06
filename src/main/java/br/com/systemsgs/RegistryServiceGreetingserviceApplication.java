8c9c817382c3c42964431423d9bf73a0f00d31af
GuilhermeJWT <guiromanno@gmail.com>
2021-06-06 08:28:48 -0300

    Configuraçoes de Registro e Habilitando Recursos

--- src/main/java/br/com/systemsgs/RegistryServiceGreetingserviceApplication.java
+++ src/main/java/br/com/systemsgs/RegistryServiceGreetingserviceApplication.java
@@ -2,8 +2,10 @@ package br.com.systemsgs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class RegistryServiceGreetingserviceApplication {
 
 	public static void main(String[] args) {
