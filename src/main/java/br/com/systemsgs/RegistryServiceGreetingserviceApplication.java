ca2189ea548b46331e9f713381bcc20977fc53be
GuilhermeJWT <guiromanno@gmail.com>
2021-06-06 08:24:12 -0300

    Setup Inicial do Projeto Server

---
+++ src/main/java/br/com/systemsgs/RegistryServiceGreetingserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceGreetingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceGreetingserviceApplication.class, args);
+	}
+
+}
